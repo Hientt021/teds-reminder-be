d3d617e0370773bef4861044704c0b0ec2cd753b
Ted Tran <ttran@calibrated.io>
2024-03-21 05:14:36 +0700

    init

--- src/app.js
+++ src/app.js
@@ -8,38 +8,13 @@ const { connectDataBase } = require("./mongo/index.js");
 const app = express();
 const port = 5000;
 const endPoint = "/api/v1";
+connectDataBase();
 
-// config
-
-app.set("view engine", "ejs");
-app.set("views", path.join(__dirname, "views"));
-app.get("/", (req, res) => res.send("Express on Vercel"));
-// middleware
+app.get("/", (req, res) => res.send("New Express on Vercel"));
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
-app.use(
-  session({
-    resave: false, // don't save session if unmodified
-    saveUninitialized: false, // don't create session until something stored
-    secret: "shhhh, very secret",
-  })
-);
 
-// Session-persisted message middleware
-
-app.use(function (req, res, next) {
-  var err = req.session.error;
-  var msg = req.session.success;
-  delete req.session.error;
-  delete req.session.success;
-  res.locals.message = "";
-  if (err) res.locals.message = '<p class="msg error">' + err + "</p>";
-  if (msg) res.locals.message = '<p class="msg success">' + msg + "</p>";
-  next();
-});
-
-connectDataBase();
 app.use(endPoint, users);
 app.use(endPoint, tasks);
 
