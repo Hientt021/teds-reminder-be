5ed2d66be3cf757928d050d21327000988be007a
Ted Tran <ttran@calibrated.io>
2024-03-21 05:09:24 +0700

    init

--- api/index.js
+++ api/index.js
@@ -1,3 +1,8 @@
-const app = require(".");
+const express = require("express");
+const app = express();
+
+app.get("/", (req, res) => res.send("Express on Vercel"));
+
+app.listen(3000, () => console.log("Server ready on port 3000."));
 
 module.exports = app;
