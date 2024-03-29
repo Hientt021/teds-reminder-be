cd8a9770d4b1eafb0f790227ac1300ae084e6133
Ted Tran <ttran@calibrated.io>
2024-03-30 02:36:28 +0700

    install

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ const app = express();
 const port = process.env.PORT || 5000;
 const endPoint = "/api/v1";
 const uri = process.env.MONGOOSE_DB;
-mongoose.connect(uri);
+mongoose.connect(uri).then((data) => console.log("trigger"));
 
 app.use(express.urlencoded({ extended: false }));
 app.use(cors());
