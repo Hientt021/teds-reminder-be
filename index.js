72ee7ca51a3ed69276efe5f8f60a55a80d8a2d9c
Ted Tran <ttran@calibrated.io>
2024-04-04 20:41:55 +0700

    .

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ import middlewareController from "./controllers/middlewareController.js";
 dotenv.config();
 const app = express();
 
-const port = process.env.PORT || 5000;
+const port = normalizePort(process.env.PORT || "5000");
 const endPoint = "/api/v1";
 const uri = process.env.MONGOOSE_DB;
 mongoose.connect(uri).then((data) => console.log("Connected to MongoDB"));
@@ -41,6 +41,47 @@ app.use(
   projects
 );
 
-app.listen(port, () => {
-  console.log(`App listening on port ${port}`);
+const normalizePort = (val) => {
+  const port = parseInt(val, 10);
+
+  if (isNaN(port)) {
+    return val;
+  }
+  if (port >= 0) {
+    return port;
+  }
+  return false;
+};
+app.set("port", port);
+
+const errorHandler = (error) => {
+  if (error.syscall !== "listen") {
+    throw error;
+  }
+  const address = server.address();
+  const bind =
+    typeof address === "string" ? "pipe " + address : "port: " + port;
+  switch (error.code) {
+    case "EACCES":
+      console.error(bind + " requires elevated privileges.");
+      process.exit(1);
+      break;
+    case "EADDRINUSE":
+      console.error(bind + " is already in use.");
+      process.exit(1);
+      break;
+    default:
+      throw error;
+  }
+};
+
+const server = http.createServer(app);
+
+server.on("error", errorHandler);
+server.on("listening", () => {
+  const address = server.address();
+  const bind = typeof address === "string" ? "pipe " + address : "port " + port;
+  console.log("Listening on " + bind);
 });
+
+server.listen(port);
