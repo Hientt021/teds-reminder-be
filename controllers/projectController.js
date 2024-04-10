c12a3fe47c2aac6cc07cbe2020de04ad034b8611
Ted Tran <ttran@calibrated.io>
2024-04-10 18:04:29 +0700

    get project sort

--- controllers/projectController.js
+++ controllers/projectController.js
@@ -5,7 +5,9 @@ const projectController = {
   getAllProject: async (req, res) => {
     try {
       const { _id } = req.user;
-      const projects = await ProjectModel.find({ created_by: _id });
+      const projects = await ProjectModel.find({ created_by: _id }).sort({
+        _id: -1,
+      });
       if (projects) res.status(200).json(successResponse(projects, "Success"));
     } catch (e) {
       res.status(500).json(errorResponse("Can not get all projects"));
