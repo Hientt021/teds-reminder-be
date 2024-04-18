e5963de75b3655afb6310b5467ec7ac8d1a1857e
Ted Tran <ttran@calibrated.io>
2024-04-19 04:22:27 +0700

    .

--- models/userModel.js
+++ models/userModel.js
@@ -10,6 +10,9 @@ const UserSchema = new mongoose.Schema(
       type: String,
       required: true,
     },
+    position: {
+      type: String,
+    },
     email: {
       type: String,
       required: true,
